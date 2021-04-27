97ffe517dfb607c5850927e70f5df1f2242d2101
Michelle Andersson <michelleandersson@live.com>
2021-04-27 09:41:40 +0200

    create handleClick function

--- src/components/Game.js
+++ src/components/Game.js
@@ -9,7 +9,16 @@ const Game = () => {
     const winner = calculateWinner(board);
 
 
-    const handleClick = () => {
+    const handleClick = i => {
+        //the [...board] makes a shallow copy of the board array
+        //'...' = spread (JSX syntax)
+        const boardCopy = [...board]; 
+        //if user clicks an occupied square or if the game is won, return 
+        if (winner || boardCopy[i]) return; 
+        // put an x or an 0 in the clicked square
+        boardCopy[i] = xIsNext ? 'X' : '0'; 
+        setBoard(boardCopy); 
+        setXisNext(!xIsNext); 
 
     }
 
